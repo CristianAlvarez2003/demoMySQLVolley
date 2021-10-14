b99f99c601a081c6e15d7f71129ccc830d6d3a4c
CristianAlvarez2003 <cris0192776@gmail.com>
2021-10-14 11:20:02 -0600

    carpeta nueva

---
+++ app/src/main/java/com/itca/demomysql_volley/ui/Acerdade/AcercadeFragment.java
@@ -0,0 +1,24 @@
+package com.itca.demomysql_volley.ui.Acerdade;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.itca.demomysql_volley.R;
+
+
+public class AcercadeFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_acercade, container, false);
+    }
+}
